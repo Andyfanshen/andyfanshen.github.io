1f382d801563d98552fb08f786e4f518cbbe1fd6
AndyFanshen <33785908+Andyfanshen@users.noreply.github.com>
2022-06-02 20:08:19 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ function setUserName() {
         setUserName();
     } else {
         localStorage.setItem('name', myName);
-        myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+        myHeading.textContent = 'Hello, ' + myName;
     }
 }
 
@@ -26,7 +26,7 @@ if (!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+    myHeading.textContent = 'Hello, ' + storedName;
 }
 
 myButton.onclick = function () {
